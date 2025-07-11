910b3b491709642f7d81f625b9db56075ae886a3
Danush Gopinath <danushgopinath8502@gmail.com>
2025-07-11 11:29:43 -0400

    Binary Search Easy 35

---
+++ Binary Search/Easy/35. Search Insert Position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            cout << "mid:" << mid << "  value:" << nums[mid] << endl;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        cout << "Insert position: " << low << endl;
+        return low; 
+    }
+};
